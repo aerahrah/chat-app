dbd54259d68f1c09f8b3e374cd944b107463273a
Marc Ivan Guillermo <61181505+aerahrah@users.noreply.github.com>
2023-09-07 18:47:16 +0800

    Add socket.io and message model

---
+++ server/models/Message.mjs
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const messageSchema = new mongoose.Schema({
+  content: {
+    type: String,
+    required: true,
+  },
+  sender: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Users",
+    required: true,
+  },
+  recipient: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Users",
+    required: true,
+  },
+  timestamp: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+export default mongoose.model("Message", messageSchema);
