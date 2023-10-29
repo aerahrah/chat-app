d3859e7f7eae9ab8f81be4b2e1b9fff023149786
Marc Ivan Guillermo <61181505+aerahrah@users.noreply.github.com>
2023-10-29 21:19:45 +0800

    Add react hook forms, add validation schema on client

--- client/src/components/auth/signup.jsx
+++ client/src/components/auth/signup.jsx
@@ -2,20 +2,39 @@ import { useMutation } from "react-query";
 import { useState } from "react";
 import { signUp } from "../api/authAPI.js";
 import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
 
 const SignUp = () => {
   const signUpMutation = useMutation(signUp);
-  const [userInfo, setUserInfo] = useState({
-    email: "",
-    username: "",
-    password: "",
-    firstName: "",
-    lastName: "",
+  const schema = yup.object().shape({
+    email: yup
+      .string()
+      .required("Email is required")
+      .email("Invalid email format"),
+    username: yup.string().required("Username is required"),
+    password: yup
+      .string()
+      .required("Password is required")
+      .min(8, "Password must be at least 8 characters"),
+    firstName: yup.string().required("First Name is required"),
+    lastName: yup.string().required("Last Name is required"),
   });
 
-  const handleSignUp = async () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
+
+  const handleSignUp = async (data) => {
     try {
-      await signUpMutation.mutateAsync(userInfo);
+      console.log(data);
+      const response = await signUpMutation.mutateAsync(data);
+      console.log(response);
     } catch (error) {
       console.log(error);
     }
@@ -26,75 +45,40 @@ const SignUp = () => {
         <h1 className="text-7xl font-black text-blue-950 dark:text-blue-500">
           ChatMe
         </h1>
-        <div className="flex flex-col gap-4 bg-white shadow-lg p-6 rounded-md text-lg dark:bg-neutral-800">
+        <form
+          className="flex flex-col gap-4 bg-white shadow-lg p-6 rounded-md text-lg dark:bg-neutral-800"
+          onSubmit={handleSubmit(handleSignUp)}
+        >
           <div className="flex gap-4 justify-between ">
             <input
               className="outline outline-1 outline-neutral-300 text-neutral-700 bg-neutral-50 rounded focus:outline-blue-500 block w-full p-2 dark:bg-neutral-800 dark:outline-neutral-700 dark:text-neutral-300"
-              type="text"
-              value={userInfo.firstName}
               placeholder="First Name"
-              required
-              onChange={(e) =>
-                setUserInfo((prevVal) => ({
-                  ...prevVal,
-                  firstName: e.target.value,
-                }))
-              }
+              {...register("firstName")}
             />
-
+            <p>{errors.firstName?.message}</p>
             <input
               className="outline outline-1 outline-neutral-300 text-neutral-700 bg-neutral-50 rounded focus:outline-blue-500 block w-full p-2 dark:bg-neutral-800 dark:outline-neutral-700 dark:text-neutral-300"
-              type="text"
-              value={userInfo.lastName}
               placeholder="Last Name"
-              required
-              onChange={(e) =>
-                setUserInfo((prevVal) => ({
-                  ...prevVal,
-                  lastName: e.target.value,
-                }))
-              }
+              {...register("lastName")}
             />
           </div>
           <div className="flex flex-col gap-4">
             <input
               className="outline outline-1 outline-neutral-300 text-neutral-700 bg-neutral-50 rounded focus:outline-blue-500 block w-full p-2 dark:bg-neutral-800 dark:outline-neutral-700 dark:text-neutral-300"
-              type="text"
-              value={userInfo.username}
               placeholder="Username"
-              required
-              onChange={(e) =>
-                setUserInfo((prevVal) => ({
-                  ...prevVal,
-                  username: e.target.value,
-                }))
-              }
+              {...register("username")}
             />
             <input
               className="outline outline-1 outline-neutral-300 text-neutral-700 bg-neutral-50 rounded focus:outline-blue-500 block w-full p-2 dark:bg-neutral-800 dark:outline-neutral-700 dark:text-neutral-300"
-              type="text"
-              value={userInfo.email}
               placeholder="Email"
-              required
-              onChange={(e) =>
-                setUserInfo((prevVal) => ({
-                  ...prevVal,
-                  email: e.target.value,
-                }))
-              }
+              type="email"
+              {...register("email")}
             />
             <input
               className="outline outline-1 outline-neutral-300 text-neutral-700 bg-neutral-50 rounded focus:outline-blue-500 block w-full p-2 dark:bg-neutral-800 dark:outline-neutral-700 dark:text-neutral-300"
-              type="password"
-              value={userInfo.password}
               placeholder="Password"
-              required
-              onChange={(e) =>
-                setUserInfo((prevVal) => ({
-                  ...prevVal,
-                  password: e.target.value,
-                }))
-              }
+              type="password"
+              {...register("password")}
             />
             <button className="text-base text-blue-500">
               <Link to="/signin">Go to login</Link>
@@ -102,14 +86,12 @@ const SignUp = () => {
           </div>
           <hr className="h-[1px] bg-neutral-300 dark:bg-neutral-800" />
           <div className="flex flex-col ">
-            <button
+            <input
               className="p-2 mt-4 text-green-50 transition duration-100  bg-green-500 hover:bg-green-600 min-w-48 w-[60%] mx-auto rounded-md"
-              onClick={handleSignUp}
-            >
-              Sign up
-            </button>
+              type="submit"
+            />
           </div>
-        </div>
+        </form>
       </div>
     </div>
   );
